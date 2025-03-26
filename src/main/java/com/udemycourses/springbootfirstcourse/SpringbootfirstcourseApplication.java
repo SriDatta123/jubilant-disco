f5a2cc53174c810bb703236a28f720bb38c37bd6
sri datta <sri.datta121@gmail.com>
2025-03-26 21:42:37 +0530

    first commit

---
+++ src/main/java/com/udemycourses/springbootfirstcourse/SpringbootfirstcourseApplication.java
@@ -0,0 +1,13 @@
+package com.udemycourses.springbootfirstcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirstcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfirstcourseApplication.class, args);
+	}
+
+}
